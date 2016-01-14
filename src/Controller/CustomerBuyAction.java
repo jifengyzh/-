337416fa2eef252905400c58baab60204b512cdd
Seanqin86 <Sean@10.0.0.3>
2016-01-13 21:35:43 -0500

    9:35Pm qin

--- src/Controller/CustomerBuyAction.java
+++ src/Controller/CustomerBuyAction.java
@@ -4,10 +4,15 @@ import java.text.DecimalFormat;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.mybeans.form.FormBeanException;
 import org.mybeans.form.FormBeanFactory;
 
+import Model.CustomerDAO;
+import Model.FundDAO;
+import Model.FundPriceHistoryDAO;
+import Model.TransactionDAO;
 import databean.CustomerBean;
 import model.Model;
 
@@ -34,48 +39,49 @@ public class CustomerBuyAction extends Action{
 	@Override
 	public String perform(HttpServletRequest request) {
 		// TODO Auto-generated method stub
+		HttpSession session = request.getSession(false);
 		List<String> errors = new ArrayList<String>();
 		request.setAttribute("errors", errors);
 		
 		try {
-		String fundName = request.getParameter("getFundName");
-		if (fundName != null) request.setAttribute("getFundName", fundName);
-		
-		FundGeneralInfoBean[] fundGeneralList = fundPriceHistoryDAO.getAllFundsGeneralInfo();
-		request.setAttribute("fundGeneralList", fundGeneralList);
-		
-		int customerId = (Integer)request.getSession(false).getAttribute("customerId");
-		CustomerBean customer = customerDAO.read(customerId);
-		formatter = new DecimalFormat("#,##0.00");
-		String cash = formatter.format(customer.getCash());
-		request.setAttribute("cash", cash);		
-		
-		BuyFundForm form = formBeanFactory.create(request);
-		if (!form.isPresent()) {
-			return "customerbuyfund.jsp";
-		}
-		errors.addAll(form.getValidationErrors());
-		if (errors.size() != 0) {
-			return "customerbuyfund.jsp";
-		}
-		
-		request.setAttribute("form", form);
-		
-		FunBean fund = fundDAO.read(form.getFundName());
-		if (fund == null) {
-			errors.add("Fund does not exist!");
-			return "customerbuyfund.jsp";
-		}
-		
-		double amount = form.getAmountAsDouble();
-		double cash = customer.getCash();
-		
-		transactionDAO.buyFund(customerId, fund.getFundId(), amount);
-		
-		request.setAttribute("message", "Thank you! your request to buy " + form.getFundName() + 
-		"has been queued until transaction day");
-		
-		return "customercomfirmation.jsp";
+			String fundName = request.getParameter("getFundName");
+			if (fundName != null) request.setAttribute("getFundName", fundName);
+			
+			FundGeneralInfoBean[] fundGeneralList = fundPriceHistoryDAO.getAllFundsGeneralInfo();
+			request.setAttribute("fundGeneralList", fundGeneralList);
+			
+			int customerId = (Integer)session.getAttribute("customerId");
+			CustomerBean customer = customerDAO.read(customerId);
+			formatter = new DecimalFormat("#,##0.00");
+			String cash = formatter.format(customer.getCash());
+			request.setAttribute("cash", cash);		
+			
+			BuyFundForm form = formBeanFactory.create(request);
+			if (!form.isPresent()) {
+				return "customerbuyfund.jsp";
+			}
+			errors.addAll(form.getValidationErrors());
+			if (errors.size() != 0) {
+				return "customerbuyfund.jsp";
+			}
+			
+			request.setAttribute("form", form);
+			
+			FunBean fund = fundDAO.read(form.getFundName());
+			if (fund == null) {
+				errors.add("Fund does not exist!");
+				return "customerbuyfund.jsp";
+			}
+			
+			double amount = form.getAmountAsDouble();
+			double cash = customer.getCash();
+			
+			transactionDAO.buyFund(customerId, fund.getFundId(), amount);
+			
+			request.setAttribute("message", "Thank you! your request to buy " + form.getFundName() + 
+			"has been queued until transaction day");
+			
+			return "customercomfirmation.jsp";
 		} catch (MyDAOException e) {
 			errors.add(e.getMessage());
 			return "error.jsp";
--- src/Controller/CustomerPwdChangeAction.java
+++ src/Controller/CustomerPwdChangeAction.java
@@ -1,10 +1,79 @@
 package Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.mybeans.form.FormBeanException;
 import org.mybeans.form.FormBeanFactory;
 
-public class CustomerPwdChangeAction {
+import Model.CustomerDAO;
+import Model.Model;
+import databean.CustomerBean;
+import formbean.ChangePasswordForm;
+
+public class CustomerPwdChangeAction extends Action{
+
+
+
 	private FormBeanFactory<ChangePasswordForm> formBeanFactory = 
 			FormBeanFactory<FormBean>.getInstance(ChangePasswordForm.class);
 	
 	private CustomerDAO customerDAO;
+
+
+	public CustomerPwdChangeAction(Model model) {
+		customerDAO = model.getCustomerDAO();
+	}
+	
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "customer_changepwd.do";
+	}
+
+
+	@Override
+	public String perform(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession(false);
+		List<String> errors = new ArrayList<String>();
+		request.setAttribute("errors", errors);
+		
+		try {
+			ChangePasswordForm form = formBeanFactory.create(request);
+			if (!form.isPresent()) {
+				return "customer_changepwd.jsp";
+			}
+			
+			errors.addAll(form.getValidationErrors());
+			if (errors.size() != 0) {
+				return "customer_changepwd.jsp";
+			}
+			
+			synchronized(this) {
+				CustomerBean customer = customerDAO.read((Integer) session.getAttribute("customerId"));
+				
+				if(!customer.checkPassword(form.getOldPassword())){
+					errors.add("Incorrect Password!! Please re-enter your current password.");
+					return "customer_changepwd.jsp";
+				}
+				
+				customerDAO.changePassword(customer.getCustomerId(), form.getNewPassword());
+				
+				
+			}
+			request.setAttribute("message", "Password changed!!!!");
+			return "customer-confirmation.jsp";
+			
+		} catch (MyDAOException e) {
+			errors.add(e.toString());
+			return "error.jsp";
+		} catch (FormBeanException e) {
+			errors.add(e.toString());
+			return "error.jsp";
+		}
+	}
 }
