71b7f1d71c98fe5adad354802d1724489d8fd760
Seanqin86 <Sean@QinShawns-MacBook-Pro.local>
2016-01-14 19:27:21 -0500

    Jan 14 7:27pm Qin

---
+++ src/Controller/VisitorResearchFundAction.java
@@ -0,0 +1,60 @@
+package Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import FormBean.FundInfoBean;
+import Model.FundDAO;
+import Model.FundPriceHistoryDAO;
+import Model.Model;
+import databean.FundPriceHistoryBean;
+
+public class VisitorResearchFundAction extends Action {
+	
+	private FundDAO fundDAO;
+	private FundPriceHistoryDAO fundPriceHistoryDAO;
+	
+	public VisitorResearchFundAction(Model model) {
+		fundDAO = model.getFundDAO();
+		fundPriceHistoryDAO = model.getFundPriceHistoryDAO();
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "visitor_research_fund.do";
+	}
+
+	@Override
+	public String perform(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		List<String> errors = new ArrayList<String>();
+		request.setAttribute("errors", errors);
+		
+		Integer fundId = 0;
+		if (request.getParameter("fundId") != null) {
+			fundId = Integer.parseInt(request.getParameter("fundId"));
+		}
+		
+		try {
+			FundInfoBean[] fundInfoBeanList = fundPriceHistoryDAO.getAllFundsInfo();
+			request.setAttribute("fundInfoBeanList", fundInfoBeanList);
+			
+			if (fundId != null) {
+				FundPriceHistoryBean[] fundPriceList = fundPriceHistoryDAO.getFundPriceHistory(fundId);
+				request.setAttribute("fundPriceList", fundPriceList);
+				request.setAttribute("currentFundName", request.getParameter("fundName"));
+			}
+			
+			
+			return "visitor_research_fund.jsp";
+		} catch(MyDAOException e) {
+			errors.add(e.getMessage());
+			return "errors.jsp";
+					
+		}
+	}
+
+}
---
+++ src/FormBean/FundInfoBean.java
@@ -0,0 +1,5 @@
+package FormBean;
+
+public class FundInfoBean {
+
+}
