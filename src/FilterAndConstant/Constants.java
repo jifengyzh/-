44ff28ff51121bf21ad107df97161cb0489ebb55
Seanqin86 <Sean@10.0.0.3>
2016-01-17 15:49:53 -0500

    Jan 17 3:49pm Qin

---
+++ src/FilterAndConstant/Constants.java
@@ -0,0 +1,81 @@
+package FilterAndConstant;
+
+public class Constants {
+	public static final String[] visitorActions = new String[] {
+			"visitor_buy_action.do",
+			"visitor_deposit_check.do",
+			"visitor_login.do",
+			"visitor_view_account.do",
+			"visitor_view_fund_price_history.do",
+			"visitor_change_pwd.do",
+			"visitor_research_fund.do",
+			"visitor_sell_fund.do",
+			"visitor_view_transaction_history.do",
+			"logout.do",
+			
+	}
+	public static final String[] employeeActions = new String[] {
+			"admin_new_account.do",
+			"logout.do",
+	}
+	
+	public static final String[] visitorJSP  = new String[] {
+			"visitor_buy_action.jsp",
+			"visitor_comfirmation.jsp",
+			"visitor_deposit_check.jsp",
+			"visitor_deposit_check_success.jsp",
+			"visitor_view_account.jsp",
+			"visitor_change_pwd.jsp",
+			"visitor-confirmation.jsp",
+			"visitor_research_fund.jsp",
+			"visitor_sell_fund.jsp"
+			
+			
+	}
+	
+	public static final String[] employeeJSP = new String[] {
+			"admin_new_account.jsp",
+			"admin_new_account_confirmation.jsp",
+			"admin_main_panel.jsp"
+	}
+	
+	public static final String logoutDo = new String("logout.do");
+	public static final String mainPage = new String("index.jsp");
+	public static final String errorJsp = new String("errors.jsp");
+	
+	public static final String visitorLogAction = new String("visitor_login.do");
+	
+	public static final String visitorBuyAction = new String("visitor_buy_action.do");
+	public static final String visitorBuyJsp = new String("visitor_buy_action.jsp");
+	public static final String visitorBuyConfirmJsp = new String("visitor_comfirmation.jsp");
+	
+	public static final String visitorDepositAction = new String("visitor_deposit_check.do");
+	public static final String visitorDepositeJsp = new String("visitor_deposit_check.jsp");
+	public static final String visitorDepositConfirmJsp = new String("visitor_deposit_check_success.jsp");
+	
+	
+	public static final String visitorViewAccountAction = new String("visitor_view_account.do");
+	public static final String visitorViewFundAction = new String("visitor_view_fund_price_history.do");
+	public static final String visitorViewAccountJsp = new String("visitor_view_account.jsp");
+	
+	public static final String visitorChangePwdAction = new String("visitor_change_pwd.do");
+	public static final String visitorChangePwdJsp = new String("visitor_change_pwd.jsp");
+	public static final String visitorChangePwdConfirmJsp = new String("visitor-confirmation.jsp");
+	
+	public static final String visitorResearchFundAction = new String("visitor_research_fund.do");
+	public static final String visitorResearchFundJsp = new String("visitor_research_fund.jsp");
+	
+	public static final String visitorSellAction = new String("visitor_sell_fund.do");
+	public static final String visitorSellJsp = new String("visitor_sell_fund.jsp");
+	
+	public static final String visitorViewTransHistoryAction= new String("visitor_view_transaction_history.do");
+	public static final String visitorViewTransHistoryJsp = new String("visitor_view_transaction_history.jsp");
+	
+	
+	
+	public static final String adminMainPanel = new String("admin_main_panel.jsp");
+	public static final String adminNewAccAction = new String("admin_new_account.do");
+	public static final String adminNewAccJsp = new String("admin_new_account.jsp");
+	public static final String adminNewAccConfirmJsp = new String("admin_new_account_confirmation.jsp");
+	
+}
