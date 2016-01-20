10e1785a4a1787970c8a8f5278ee8832d7e008e0
elaineliu17 <elaineliu17@users.noreply.github.com>
2016-01-20 15:39:06 -0500

    Update VisitorBean.java

--- src/databean/VisitorBean.java
+++ src/databean/VisitorBean.java
@@ -1,158 +1,76 @@
 package databean;
 
+import org.genericdao.PrimaryKey;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Date;
 import java.util.Random;
 
+@PrimaryKey("VisitorId")
 public class VisitorBean {
-	private int visitorId;
-	private String userName;
-	private String firstName;
-	private String lastName;
-	private String password;
-	private String addrLine1;
-	private String addrLine2;
-	private String city;
-	private String state;
-	private int zip;
-	private double cash;
-	private Date lastTradeDate;
-
-	public double getCash() {
-		return cash;
-	}
-
-	public void setCash(double cash) {
-		this.cash = cash;
-	}
-
-	public Date getLastTradeDate() {
-		return lastTradeDate;
-	}
-
-	public void setLastTradeDate(Date lastTradeDate) {
-		this.lastTradeDate = lastTradeDate;
-	}
-
-
-	private int salt = 0;
-	
-	public boolean checkPassword(String password) {
-		return this.password.equals(hash(password));
-	}
-	
-	public void setSalt(int salt){
-		this.salt = salt;
-	}
-	
-	public void setDirectPassword(String password){
-		this.password = password;
-	}
-	
-	public void setPassword(String password) {
-		salt = newSalt();
-		this.password = hash(password);
-	}
-	
-	public int getSalt() {
-		return salt;
-	}
-
-	public int getVisitorId() {
-		return visitorId;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public String getAddrLine1() {
-		return addrLine1;
-	}
-
-	public String getAddrLine2() {
-		return addrLine2;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public int getZip() {
-		return zip;
-	}
-
+	private long      visitorId;
+	private String    userName;
+//	private String    hashedPassword = "*";
+//	private int       salt           = 0;
+	private String    password;
+	private String    firstName;
+	private String    lastName;
+	private String    addrLine1;
+	private String    addrLine2;
+	private String    city;
+	private String    state;
+	private String    zip;
+	private long      cash;
+//	private long      availableCash;
 	
 
-	public void setVisitorId(int visitorId) {
-		this.visitorId = visitorId;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public void setAddrLine1(String addrLine1) {
-		this.addrLine1 = addrLine1;
-	}
-
-	public void setAddrLine2(String addrLine2) {
-		this.addrLine2 = addrLine2;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public void setZip(int zip) {
-		this.zip = zip;
-	}
-
+	public long      getVisitorId()      { return visitorId;      }
+	public String    getUserName()        { return userName;        }
+	public String    getPassword()        { return password;        }
+	public String    getFirstName()       { return firstName;       }
+	public String    getLastName()        { return lastName;        }
+	public String    getAddrLine1()       { return addrLine1;       }
+	public String    getAddrLine2()       { return addrLine2;       }
+	public String    getCity()            { return city;            }
+	public String    getState()           { return state;           }
+	public String    getZip()             { return zip;             }
+	public long      getCash()            { return cash;            }
+//	public long      getAvailableCash()   { return availableCash;   }
 	
+	public int     hashCode()          { return userName.hashCode(); }	
 	
+	public void   setVisitorId(long l)       { visitorId = l;       }
+	public void   setUserName(String s)       { userName = s;         }
+	public void   setPassword(String s)       { password = s;         }
+	public void   setFirstName(String s)      { firstName = s;        }
+	public void   setLastName(String s)       { lastName = s;         }
+	public void   setAddrLine1(String s)      { addrLine1 = s;        }
+	public void   setAddrLine2(String s)      { addrLine2 = s;        }
+	public void   setCity(String s)           { city = s;             }
+	public void   setState(String s)          { state = s;            }
+	public void   setZip(String s)            { zip = s;              }
+	public void   setCash(long l)             { cash = l;             }
+//	public void   setAvailableCash(long l)    { availableCash = l;    }
+/*	
+ * 
+ * 	public boolean checkPassword(String password) {
+		return hashedPassword.equals(hash(password));
+	}
+	
+ * 
 	private String hash(String clearPassword) {
+		if (salt == 0) return null;
 		MessageDigest md = null;
 		try {
 		  md = MessageDigest.getInstance("SHA1");
 		} catch (NoSuchAlgorithmException e) {
 		  throw new AssertionError("Can't find the SHA1 algorithm in the java.security package");
 		}
-
 		String saltString = String.valueOf(salt);
 		
 		md.update(saltString.getBytes());
 		md.update(clearPassword.getBytes());
 		byte[] digestBytes = md.digest();
-
 		// Format the digest as a String
 		StringBuffer digestSB = new StringBuffer();
 		for (int i=0; i<digestBytes.length; i++) {
@@ -162,12 +80,11 @@ public class VisitorBean {
 		  digestSB.append(Integer.toHexString(lowNibble));
 		}
 		String digestStr = digestSB.toString();
-
 		return digestStr;
 	}
-	
 	private int newSalt() {
 		Random random = new Random();
-		return random.nextInt(8192)+1;  // salt cannot be zero, except for uninitialized password
+		return random.nextInt(8192)+1;  // salt cannot be zero
 	}
+*/
 }
