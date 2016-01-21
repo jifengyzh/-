e91710d48dbb6bccd386fc4a4572c9dd40688872
honiess <yawenz@andrew.cmu.edu>
2016-01-21 17:55:05 -0500

    2016年01月21日17:55:01 Arwen

--- src/controller/EmployeeCreateEmployeeAccAction.java
+++ src/controller/EmployeeCreateEmployeeAccAction.java
@@ -4,10 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
-import model.EmployeeDAO;
-import model.Model;
 
 import org.genericdao.RollbackException;
 import org.mybeans.form.FormBeanException;
@@ -16,17 +12,19 @@ import org.mybeans.form.FormBeanFactory;
 import FilterAndConstant.Constants;
 import databean.EmployeeBean;
 import formbean.EmployeeCreateNewEmployeeAccForm;
+import model.EmployeeDAO;
+import model.Model;
 
+public class EmployeeCreateEmployeeAccAction extends Action {
 
-public class EmployeeCreateEmployeeAccAction extends Action{
-
-	private FormBeanFactory<EmployeeCreateNewEmployeeAccForm> createEmployeeAccountFormFactory = FormBeanFactory.getInstance(EmployeeCreateNewEmployeeAccForm.class);
+	private FormBeanFactory<EmployeeCreateNewEmployeeAccForm> createEmployeeAccountFormFactory = FormBeanFactory
+			.getInstance(EmployeeCreateNewEmployeeAccForm.class);
 	private EmployeeDAO employeeDAO;
-	
-	public EmployeeCreateEmployeeAccAction(Model model){
-		employeeDAO = model.getAdminDAO();
+
+	public EmployeeCreateEmployeeAccAction(Model model) {
+		employeeDAO = model.getEmployeeDAO();
 	}
-	
+
 	@Override
 	public String getName() {
 		return Constants.employeeCreateEmployeeAccAction;
@@ -36,6 +34,7 @@ public class EmployeeCreateEmployeeAccAction extends Action{
 	public String perform(HttpServletRequest request) {
 		List<String> errors = new ArrayList<String>();
         request.setAttribute("errors",errors);
+        request.setAttribute("success", null);
         try {
         	EmployeeCreateNewEmployeeAccForm form = createEmployeeAccountFormFactory.create(request);
 	        request.setAttribute("form",form);
@@ -49,10 +48,8 @@ public class EmployeeCreateEmployeeAccAction extends Action{
 	            return Constants.employeeCreateEmployeeAccJsp;
 	        }
 	
-	        
-	        synchronized (this) {
-	        	EmployeeBean employee = employeeDAO.lookup(form.getUserName());
-		       	if (employee != null) {
+	        EmployeeBean employee = employeeDAO.read(form.getUserName());
+		      if (employee != null) {
 		       		errors.add("Existing Username");
 	                return Constants.employeeCreateEmployeeAccJsp;
 		       	}
@@ -63,11 +60,9 @@ public class EmployeeCreateEmployeeAccAction extends Action{
 		       	employee.setFirstName(form.getFirstName());
 		       	employee.setLastName(form.getLastName());
 		       	employeeDAO.create(employee);
-	        }
         
-	        request.setAttribute("message","Account successfully created for " + "<b>" + form.getUserName() + "</b>");
-	       	
-			return Constants.employeeConfirmJsp;
+	        request.setAttribute("success","success")
+			return Constants.employeeCreateEmployeeAccJsp;
 
         } catch (FormBeanException e) {
         	errors.add(e.getMessage());
--- src/databean/EmployeeBean.java
+++ src/databean/EmployeeBean.java
@@ -13,16 +13,91 @@ public class EmployeeBean {
 	private String passWord;
 	private String firstName;
 	private String lastName;
-	
-	public String getUserName()			{ return userName;		}
-	public String getPassWord()			{ return passWord;		}
-	public String getFirstName()			{ return firstName;		}
-	public String getLastName()			{ return lastName;		}
-	
-	public void setUserName(String s)		{ this.userName	= s;	}
-	public void setPassWord(String s)		{ this.passWord = s;	}
-	public void setFirstName(String s)		{ this.firstName = s;	}
-	public void setLastName(String s)		{ this.lastName = s;	}
-	
-}	
-	
+	private int salt = 0;
+	private String hashedPassword = "*";
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public String getPassWord() {
+		return passWord;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public int getSalt() {
+		return salt;
+	}
+
+	public void setUserName(String s) {
+		this.userName = s;
+	}
+
+	public void setHashedPassword(String x) {
+		hashedPassword = x;
+	}
+
+	public void setPassword(String s) {
+		salt = newSalt();
+		hashedPassword = hash(s);
+	}
+
+	public void setSalt(int x) {
+		salt = x;
+	}
+
+	public void setFirstName(String s) {
+		this.firstName = s;
+	}
+
+	public void setLastName(String s) {
+		this.lastName = s;
+	}
+
+	public boolean checkPassword(String password) {
+		return hashedPassword.equals(hash(password));
+	}
+
+	private String hash(String clearPassword) {
+		if (salt == 0)
+			return null;
+
+		MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance("SHA1");
+		} catch (NoSuchAlgorithmException e) {
+			throw new AssertionError("Can't find the SHA1 algorithm in the java.security package");
+		}
+
+		String saltString = String.valueOf(salt);
+
+		md.update(saltString.getBytes());
+		md.update(clearPassword.getBytes());
+		byte[] digestBytes = md.digest();
+
+		// Format the digest as a String
+		StringBuffer digestSB = new StringBuffer();
+		for (int i = 0; i < digestBytes.length; i++) {
+			int lowNibble = digestBytes[i] & 0x0f;
+			int highNibble = (digestBytes[i] >> 4) & 0x0f;
+			digestSB.append(Integer.toHexString(highNibble));
+			digestSB.append(Integer.toHexString(lowNibble));
+		}
+		String digestStr = digestSB.toString();
+
+		return digestStr;
+	}
+
+	private int newSalt() {
+		Random random = new Random();
+		return random.nextInt(8192) + 1; // salt cannot be zero
+	}
+
+}
