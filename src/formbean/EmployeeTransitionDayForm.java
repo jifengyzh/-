97bbff9ec746b3fd96a709b792574c7624577d78
honiess <yawenz@andrew.cmu.edu>
2016-01-21 16:06:38 -0500

    2016-01-21 16:06:34 Arwen

---
+++ src/formbean/EmployeeTransitionDayForm.java
@@ -0,0 +1,22 @@
+package formbean;
+
+import java.util.Date;
+import java.util.Map;
+
+import org.mybeans.form.FormBean;
+
+import controller.Map;
+
+public class EmployeeTransitionDayForm extends FormBean {
+
+	public Date getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Map<Integer, Long> getNewFundPrice() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
