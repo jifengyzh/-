9e4a4a65965469497546f1849946502cb200b496
honiess <yawenz@andrew.cmu.edu>
2016-01-22 10:51:51 -0500

    2016年01月22日10:51:47 Arwen

--- src/model/VisitorDAO.java
+++ src/model/VisitorDAO.java
@@ -34,16 +34,16 @@ public class VisitorDAO extends GenericDAO<VisitorBean>{
 		return visitorBean;
 	}
 	
-	public VisitorBean readVisitor(int visitorId) throws RollbackException {
-		VisitorBean[] a = match(MatchArg.equals("visitorId",visitorId));
-		VisitorBean visitorBean;
-		if (a.length == 0) {
-			visitorBean = null;
-		} else {
-			visitorBean = a[0];
-		}
-		return visitorBean;
-	}
+//	public VisitorBean readVisitor(int visitorId) throws RollbackException {
+//		VisitorBean[] a = match(MatchArg.equals("visitorId",visitorId));
+//		VisitorBean visitorBean;
+//		if (a.length == 0) {
+//			visitorBean = null;
+//		} else {
+//			visitorBean = a[0];
+//		}
+//		return visitorBean;
+//	}
 
 	
 	public void setPassword(long l, String password) throws RollbackException {
