ffa344121004d25a2e944a2aaaa7ae07e37d8a4f
honiess <yawenz@andrew.cmu.edu>
2016-01-22 10:10:53 -0500

    2016年01月22日10:10:52

--- src/controller/EmployeeViewCustomerAccountAction.java
+++ src/controller/EmployeeViewCustomerAccountAction.java
@@ -35,14 +35,12 @@ public class EmployeeViewCustomerAccountAction extends Action {
 	private PositionDAO positionDAO;
 	private FundDAO fundDAO;
 	private FundPriceHistoryDAO fundPriceHistoryDAO;
-	private TransactionDAO transactionDAO;
 
 	public EmployeeViewCustomerAccountAction(Model model) {
 		visitorDAO = model.getVisitorDAO();
 		positionDAO = model.getPositionDAO();
 		fundDAO = model.getFundDAO();
 		fundPriceHistoryDAO = model.getFundPriceHistoryDAO();
-		transactionDAO = model.getTransactionDAO();
 
 	}
 
@@ -58,64 +56,58 @@ public class EmployeeViewCustomerAccountAction extends Action {
 		try {
 			VisitorBean[] customerlist = visitorDAO.getAllCustomers();
 			request.setAttribute("customerlist", customerlist);
-			if (request.getParameter("username") != null) {
+			CustomerUserNameForm form = formBeanFactory.create(request);
+			request.setAttribute("form", form);
 
-				CustomerUserNameForm form = formBeanFactory.create(request);
-				request.setAttribute("form", form);
-
-				// If no customer is selected, show all the customers in the
-				// system.
-
-				if (!form.isPresent()) {
-					return Constants.employeeViewCustAccJsp;
-				}
-
-				// If there's error with the given username.
-				// Return to the list page.
+			// No button & link is clicked
+			if (request.getParameter("username") == null && !form.isPresent())
+				return Constants.employeeViewCustAccJsp;
+			VisitorBean visitor;
+			// link
+			if (request.getParameter("username") != null)
+				visitor = visitorDAO.read(request.getParameter("username"));
+			// button
+			else {
 				errors.addAll(form.getValidationErrors());
 				if (errors.size() != 0) {
 					return Constants.employeeViewCustAccJsp;
 				}
-
-				VisitorBean visitor = visitorDAO.read(form.getUsername());
-				if (visitor == null) {
-					errors.add("User does not exist!");
-					return Constants.employeeViewCustAccJsp;
-				}
-
-				// Show detail info of specified customer
-				request.setAttribute("customerlist", null);
-				// Name, Address
-				int visitorId = visitor.getVisitorId();
-				request.setAttribute("customer", visitor);
-				// Cash
-				DecimalFormat formatter = new DecimalFormat("#,##0.00");
-				request.setAttribute("cash", formatter.format(visitor.getCash()));
-
-				// Fund list and value
-				PositionBean[] position = positionDAO.getPositionList(visitorId);
-				CustomerFundBean[] customerFund = new CustomerFundBean[position.length];
-				for (int i = 0; i < position.length; i++) {
-					DecimalFormat formatter1 = new DecimalFormat("#,##0.000");
-					DecimalFormat formatter2 = new DecimalFormat("#0.00");
-					int fund_id = position[i].getFundId();
-					double price = fundPriceHistoryDAO.getFundPrice(fund_id, (Date) session.getAttribute("lastdate"))
-							/ 100;
-					double shares = position[i].getShares() / 1000;
-
-					customerFund[i].setShares(formatter1.format(shares));
-					customerFund[i].setName(fundDAO.read(fund_id).getName());
-					customerFund[i].setSymbol(fundDAO.read(fund_id).getSymbol());
-					customerFund[i].setPrice(formatter1.format(price));
-					customerFund[i].setValue(formatter2.format(price * shares));
-				}
-				request.setAttribute("customerfund", customerFund);
-
+				visitor = visitorDAO.read(form.getUsername());
 			}
+			if (visitor == null) {
+				errors.add("User does not exist!");
+				return Constants.employeeViewCustAccJsp;
+			}
+			// display user's account information
+			// Name, Address
+			int visitorId = visitor.getVisitorId();
+			request.setAttribute("customer", visitor);
+			// Cash
+			DecimalFormat formatter = new DecimalFormat("#,##0.00");
+			request.setAttribute("cash", formatter.format(visitor.getCash()));
+
+			// Fund list and value
+			PositionBean[] position = positionDAO.getPositionList(visitorId);
+			CustomerFundBean[] customerFund = new CustomerFundBean[position.length];
+			for (int i = 0; i < position.length; i++) {
+				DecimalFormat formatter1 = new DecimalFormat("#,##0.000");
+				DecimalFormat formatter2 = new DecimalFormat("#0.00");
+				int fund_id = position[i].getFundId();
+				double price = fundPriceHistoryDAO.getFundPrice(fund_id, (Date) session.getAttribute("lastdate")) / 100;
+				double shares = position[i].getShares() / 1000;
+
+				customerFund[i].setShares(formatter1.format(shares));
+				customerFund[i].setName(fundDAO.read(fund_id).getName());
+				customerFund[i].setSymbol(fundDAO.read(fund_id).getSymbol());
+				customerFund[i].setPrice(formatter1.format(price));
+				customerFund[i].setValue(formatter2.format(price * shares));
+			}
+
+			request.setAttribute("customerfund", customerFund);
 			return Constants.employeeViewCustAccJsp;
 		} catch (RollbackException e) {
 			errors.add(e.getMessage());
-			return "error.jsp";
+			return Constants.employeeViewCustAccJsp;
 		} catch (FormBeanException e) {
 			errors.add(e.getMessage());
 			return Constants.employeeViewCustAccJsp;
